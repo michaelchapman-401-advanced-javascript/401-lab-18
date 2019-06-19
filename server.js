59e854daeeedff98ef62b7bc1cccfc4ae44c8772
Michael A. Chapman <chapmanm305@gmail.com>
2019-06-19 14:26:44 -0700

    Setup socket.io server in server.js and have it listening for file-save and file-error events

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// Set up socket.io server
+const io = require('socket.io')(3001);
+
+// Listen for connections to the server
+io.on('connection', socket => {
+  // Listen for file-save event
+  socket.on('file-save', payload => {
+    // emit file-save event to logger
+    socket.emit('file-save', payload);
+  });
+  
+  // Listen for file-error event
+  socket.on('file-error', payload => {
+    socket.emit('file-error', payload);
+  });
+  // emit file-error event to logger
+});
