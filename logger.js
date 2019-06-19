482968c4ba5d2311a2e6ecf4c0514bccb55bca19
Michael A. Chapman <chapmanm305@gmail.com>
2019-06-19 14:07:30 -0700

    Initial commit

---
+++ logger.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Setup socket.io-client
+
+// connect to server.js
+// Listen for file-save event
+// console.log(message);
+// Listen for file-error event
+// console.error(message)
